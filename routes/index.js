b3932f486655f329efbc947d66b8beaba0e4a1e9
Robert Bunch <rob@digitalcrafts.com>
2017-02-07 11:59:27 -0500

    Add update get route and format date

--- routes/index.js
+++ routes/index.js
@@ -40,8 +40,54 @@ router.post('/addNew', (req, res, next)=>{
 		if (error) throw error;
 		res.redirect('/');
 	});
+});
 
+/////////////EDIT GET//////////////////
+router.get('/edit/:id', (req, res, next)=>{
+	// res.send(req.params.id);
+	var selectQuery = "SELECT * FROM tasks WHERE id ="+req.params.id;
+	// res.send(selectQuery);
+	connection.query(selectQuery, (error,results,fields)=>{
+		// res.json(results);
+		// var date = results[0].task_date;
+		var days = results[0].task_date.getDate();
+		if(days < 10){
+			days = "0"+days;
+		}
+		var months = results[0].task_date.getMonth() + 1;
+		if(months < 10){
+			months = "0"+months;
+		}		
+		var years = results[0].task_date.getFullYear();
+		var mysqlDate = years + '-' + months + '-' + days;
+		results[0].task_date = mysqlDate;
+		// res.json(date);
+		res.render('edit', { task:results[0] } );
+	})
+});
 
+/////////////EDIT POST//////////////////
+router.post('/edit/:id', (req, res, next)=>{
+	// id is in the req.params
+	var id = req.params.id
+	// res.send(req.params.id);
+	// task_name and task_date are in req.body
+	var newTask = req.body.newTaskString;
+	var taskDate = req.body.newTaskDate;
+
+	var updateQuery = "UPDATE tasks SET task_name='"+newTask+"', task_date='"+taskDate+"' WHERE ID = "+id;
+	res.send(updateQuery);
+});
+
+/////////////DELETE GET//////////////////
+router.get('/delete/:id', (req, res, next)=>{
+	var selectQuery = "SELECT * FROM tasks WHERE id ="+req.params.id;
+	// res.send(req.params.id);
+});
+
+/////////////DELETE POST//////////////////
+router.post('/delete/:id', (req, res, next)=>{
+	res.send(req.params.id);
 });
 
 module.exports = router;
