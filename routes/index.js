01247c3c00865b35ab4beeea2e44a927eebb4878
Robert Bunch <rob@digitalcrafts.com>
2017-02-07 09:31:50 -0500

    Put index boilerplate together

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	//Init array as a placeholder
+	var taskArray = [];
+
+	res.render('index', { taskArray: taskArray });
 });
 
 module.exports = router;
